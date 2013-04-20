c455a38ef78ab4ecad84cc6f908a718fafe6bc9c
Eric Tan <eyt4@cornell.edu>
2013-04-20 12:53:54 -0400

    Commit message test
    -hi this is my commit message

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <title>Chordials</title>
+    <meta http-equiv="Content-type" content="text/html;charset=UTF-8">
+    <meta name="keywords" content="chordials" />
+</head>
+
+<body>
+
+</body>
+</html>
